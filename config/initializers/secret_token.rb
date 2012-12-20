3c3243fa870763190fff001347b8747f8f9b4a9d
Anders Janmyr <anders@janmyr.com>
2012-12-20 13:13:01 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Questbox::Application.config.secret_token = 'c004bdb5489baf6fb735d42036f305104a1859f00c84b881971b153ced8fae9609626b0bea3f87a2320430492bc4476c1bf247ae6004d790e4fbf58f4b2edbed'

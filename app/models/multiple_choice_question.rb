2be35f2e72a55be14c7ede52c7069e644319effb
Anders Janmyr <anders@janmyr.com>
2013-01-08 15:03:55 +0100

    Added some multiple choice validation

--- app/models/multiple_choice_question.rb
+++ app/models/multiple_choice_question.rb
@@ -2,4 +2,24 @@ class MultipleChoiceQuestion < Question
   field :alternatives, type: Array, default: []
   field :correct_alternative, type: Integer, default: 0
   attr_accessible :alternatives, :correct_alternative
+
+  before_validation :trim_blank_alternatives
+
+  validate :alternatives_must_be_more_than_one, :correct_alternative_must_be_available
+
+  def trim_blank_alternatives
+    alternatives.reject! { |a| a.blank? }
+  end
+
+  def alternatives_must_be_more_than_one
+    if alternatives.size < 2
+      errors.add(:alternatives, 'must be more than one')
+    end
+  end
+
+  def correct_alternative_must_be_available
+    if correct_alternative >= alternatives.size
+      errors.add(:correct_alternative, 'must be available')
+    end
+  end
 end
--- spec/models/multiple_choice_question_spec.rb
+++ spec/models/multiple_choice_question_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
 
 describe MultipleChoiceQuestion do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#trim_blank_alternatives' do
+    it 'removes blank alternatives' do
+      question = MultipleChoiceQuestion.new alternatives: ['a', '', nil, 'b']
+      question.trim_blank_alternatives
+      question.alternatives.should eq(['a', 'b'])
+    end
+  end
+
+  describe '#alternatives_must_be_more_than_one' do
+    it 'is valid for two' do
+      question = MultipleChoiceQuestion.new text: 'text', alternatives: ['a', 'b']
+      question.should be_valid
+    end
+
+    it 'is invalid for one' do
+      question = MultipleChoiceQuestion.new text: 'text', alternatives: ['a']
+      question.should_not be_valid
+    end
+  end
+
+  describe '#correct_alternative_must_be_available' do
+    it 'is valid correct_alternative=1 with two alternatives' do
+      question = MultipleChoiceQuestion.new(
+        text: 'text',
+        correct_alternative: 1,
+        alternatives: ['a', 'b']
+      )
+      question.should be_valid
+    end
+
+    it 'is invalid with correct_alternative=2 with two alternatives' do
+      question = MultipleChoiceQuestion.new(
+        text: 'text',
+        correct_alternative: 2,
+        alternatives: ['a', 'b']
+      )
+      question.should_not be_valid
+    end
+  end
 end
